67146150022c1be8173b8e8397430d98c899f05d
Takunda <taku@mugonat.com>
2026-03-06 13:06:47 +0200

    certificate

--- resources/views/calibrations/certificate.blade.php
+++ resources/views/calibrations/certificate.blade.php
@@ -231,7 +231,7 @@
                         @if($isSingleCompartment)
                             @php $comp = $compartments->first(); @endphp
                             <tr>
-                                <th colspan="4">
+                                <th colspan="6">
                                     COMPARTMENT 1<br>
                                     Reference Height:<br>
                                     {{ $comp->height > 0 ? number_format($comp->height, 0) : 'N/A' }} mm
@@ -239,6 +239,8 @@
                             </tr>
                             <tr class="subheader">
                                 <th>Dip (mm)</th>
+                                <th>Volume (L)</th>
+                                <th style="border-left: 2px solid #000;">Dip (mm)</th>
                                 <th>Volume (L)</th>
                                 <th style="border-left: 2px solid #000;">Dip (mm)</th>
                                 <th>Volume (L)</th>
@@ -263,7 +265,7 @@
                     </thead>
                     <tfoot>
                         <tr>
-                            <td colspan="{{ $isSingleCompartment ? 4 : $compartments->count() * 2 }}" style="border: none;">
+                            <td colspan="{{ $isSingleCompartment ? 6 : $compartments->count() * 2 }}" style="border: none;">
                                 <div class="footer-content">
                                     <p style="font-weight: bold; margin: 2px 0;">ALL DIP HEIGHT READINGS MUST BE TAKEN ON LEVEL GROUND</p>
                                     <p style="margin: 2px 0;">This certificate is valid for two calendar years from date of calibration provided the truck/tanker combination is as recorded above.</p>
@@ -277,18 +279,21 @@
                             @php
                                 $readings = $calibration->interpolations->where('compartment_number', $comp->number)->values();
                                 $totalReadings = $readings->count();
-                                $half = ceil($totalReadings / 2);
+                                $third = ceil($totalReadings / 3);
                             @endphp
-                            @for($i = 0; $i < $half; $i++)
+                            @for($i = 0; $i < $third; $i++)
                                 @php
-                                    $left = $readings->get($i);
-                                    $right = $readings->get($i + $half);
+                                    $col1 = $readings->get($i);
+                                    $col2 = $readings->get($i + $third);
+                                    $col3 = $readings->get($i + ($third * 2));
                                 @endphp
                                 <tr>
-                                    <td>{{ $left ? number_format($left->dip_mm, 0) : '' }}</td>
-                                    <td>{{ $left ? number_format($left->volume, 0, '.', '') : '' }}</td>
-                                    <td style="border-left: 2px solid #000;">{{ $right ? number_format($right->dip_mm, 0) : '' }}</td>
-                                    <td>{{ $right ? number_format($right->volume, 0, '.', '') : '' }}</td>
+                                    <td>{{ $col1 ? number_format($col1->dip_mm, 0) : '' }}</td>
+                                    <td>{{ $col1 ? number_format($col1->volume, 0, '.', '') : '' }}</td>
+                                    <td style="border-left: 2px solid #000;">{{ $col2 ? number_format($col2->dip_mm, 0) : '' }}</td>
+                                    <td>{{ $col2 ? number_format($col2->volume, 0, '.', '') : '' }}</td>
+                                    <td style="border-left: 2px solid #000;">{{ $col3 ? number_format($col3->dip_mm, 0) : '' }}</td>
+                                    <td>{{ $col3 ? number_format($col3->volume, 0, '.', '') : '' }}</td>
                                 </tr>
                             @endfor
                         @else
